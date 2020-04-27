fae690be89f0f2070ca22e574acde7344a0670c5
Glitch (dapper-cerulean-manchego) <none>
2020-04-27 08:42:43 +0000

    🏀⛪️ Updated with Glitch

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require('express');
+const app = express();
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get('/todos', (request, response) => {
+  response.send('<ul><li>Đi chợ</li><li>Nấu cơm</li> <li>Rửa bát</li></ul>');
+});
+
+// listen for requests :)
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
