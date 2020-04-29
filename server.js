ff11087926f8dc1e5e1318e38421a9d85d947b3b
Glitch (3-1-query-parameter) <none>
2020-04-29 08:00:58 +0000

    👯👵 Updated with Glitch

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+var todos = [
+  {id: 1, name: 'Đi chợ'},
+  {id: 2, name: 'Rửa bát'},
+  {id: 3, name: 'Nấu cơm'}
+];
 
 app.set('views','./views');
 app.set('view engine', 'pug');
@@ -14,9 +19,22 @@ app.get('/', (request, response) => {
   response.send('I love CodersX');
 });
 
-app.get('/todos',(req,res)=>{
-  res.render('todos');
-})
+// app.get('/todos',(req,res)=>{
+//   res.render('todos', {
+//     todos: todos
+//   });
+// });
+
+app.get('/todos', (req,res) => {
+  var q = req.query.q;
+  var matchedItems = todos.filter(function(todo){
+    return todo.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  
+  res.render('todos',{
+    todos: matchedItems
+  })
+});
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
