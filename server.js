575fa3e7ecceb1db1b466e8bff1f3ca0b0a353b8
Glitch (4-1-post-method) <none>
2020-04-30 06:15:05 +0000

    🎟🎇 Updated with Glitch

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+
+
+
 var todos = [
   {id: 1, name: 'Đi chợ'},
   {id: 2, name: 'Rửa bát'},
@@ -13,6 +17,8 @@ var todos = [
 
 app.set('views','./views');
 app.set('view engine', 'pug');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
@@ -35,6 +41,15 @@ app.get('/todos/search', (req,res) => {
     todos: matchedItems
   })
 });
+
+app.get('/todos/create', (req,res) => {
+  res.render('create');
+});
+
+app.post('/todos/create', (req,res) => {
+  todos.push(req.body);
+  res.redirect('/todos');
+})
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
