6662711dd8b4cc3f7083e7a2063ebd01236741ba
Glitch (7-2-route-param) <none>
2020-05-02 09:47:03 +0000

    ✍️🖐 Updated with Glitch

--- server.js
+++ server.js
@@ -3,70 +3,69 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-
-const adapter = new FileSync('db.json')
-const db = low(adapter)
+const bodyParser = require('body-parser');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 const shortid = require('shortid');
 
 
-var todoList = db.get('todos').value();
-// var todos = [
-//   {id: 1, name: 'Đi chợ'},
-//   {id: 2, name: 'Rửa bát'},
-//   {id: 3, name: 'Nấu cơm'}
-// ];
 
 app.set('views','./views');
 app.set('view engine', 'pug');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get('/', (req, res) => {
+  res.send('My name is Nghia');
 });
 
-app.get('/todos',(req,res)=>{
-  res.render('todos', {
-    todos: todoList
-  });
-});
-
-app.get('/todos/search', (req,res) => {
-  var q = req.query.q;
-  var matchedItems = todoList.filter(function(todo){
-    return todo.text.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-  
-  res.render('todos', {
-    todos: matchedItems
+app.get('/books', (req, res) => {
+  res.render('books',{
+    books: db.get('books').value()
   })
 });
 
-app.get('/todos/create', (req,res) => {
-  res.render('create');
+app.get('/books/add', (req,res) => {
+  res.render('add');
 });
 
-app.post('/todos/create', (req,res) => {
+app.post('/books/add', (req,res) => {
   req.body.id = shortid.generate();
-  db.get('todos')
+  db.get('books')
     .push(req.body)
     .write();
-  res.redirect('/todos');
+  res.redirect('/books');
+  
+});
+
+app.get('/books/delete/:id', (req, res) => {
+  var id = req.params.id;
+  db.get('books')
+    .remove({id: id})
+    .write()
+  res.redirect('/books')
 })
 
-app.get('/todos/:id/delete', (req, res) => {
-  var id = req.params.id;
-  db.get('todos').remove({id: id}).write();
-  res.redirect('/todos')
+app.get('/books/update', (req,res) => {
+  res.render('update');
+})
+
+app.post('/books/update', (req,res) => {
+  // console.log(req.body)
+  var title = req.body.title;
+  var newTitle = req.body.newTitle;
+  db.get('books')
+    .find({title: title})
+    .assign({title: newTitle})
+    .write()
+  res.redirect('/books');
 })
 
 // listen for requests :)
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on port " + process.env.PORT);
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
 });
