38b915a029ee7d6680f32f83823a52363c23ece2
Glitch (8-1-express-router) <none>
2020-05-05 08:50:10 +0000

    👩🐽 Updated with Glitch

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ var shortid = require('shortid');
 var db = require('./db.js');
 
 var userRoute = require('./routes/user.route');
-var bookRoute = require('./routes/book.route');
-var transactionRoute = require('./routes/transaction.route')
 
 
 
@@ -24,11 +22,52 @@ app.get('/', (req, res) => {
   res.send('My name is Nghia');
 });
 
+app.get('/books', (req, res) => {
+  res.render('books',{
+    books: db.get('books').value()
+  })
+});
 
-app.use('/books', bookRoute);
+app.get('/books/add', (req,res) => {
+  res.render('add');
+});
+
+app.post('/books/add', (req,res) => {
+  req.body.id = shortid.generate();
+  db.get('books')
+    .push(req.body)
+    .write();
+  res.redirect('/books');
+  
+  // console.log(req.body)
+  
+  
+});
+
+app.get('/books/delete/:id', (req, res) => {
+  var id = req.params.id;
+  db.get('books')
+    .remove({id: id})
+    .write()
+  res.redirect('/books')
+})
+
+app.get('/books/update', (req,res) => {
+  res.render('update');
+})
+
+app.post('/books/update', (req,res) => {
+  // console.log(req.body)
+  var title = req.body.title;
+  var newTitle = req.body.newTitle;
+  db.get('books')
+    .find({title: title})
+    .assign({title: newTitle})
+    .write()
+  res.redirect('/books');
+});
+
 app.use('/users', userRoute);
-app.use('/transactions', transactionRoute);
-
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
